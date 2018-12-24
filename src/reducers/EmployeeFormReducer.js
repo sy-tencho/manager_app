d91773eb65e66af363c28e0bd788da4f27039da7
Shota Yamazaki <sysy5867@gmail.com>
2018-12-24 19:28:17 +0900

    subtle fix

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -13,8 +13,7 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case EMPLOYEE_UPDATE:
-            return { ...state, [action.payload.props]: action.payload.value };
-
+            return { ...state, [action.payload.prop]: action.payload.value };
         case EMPLOYEE_CREATE:
             return INITIAL_STATE;
         case EMPLOYEE_SAVE_SUCCESS:
